e59813d09381fc261443f4472f5aa11eb5d03fcb
LukeyJno1 <158720503+LukeyJno1@users.noreply.github.com>
2024-10-01 19:39:08 +0100

    Add files via upload

---
+++ layout_creator/includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>CMS Management System</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="db_setup.php">Database Setup</a></li>
+        </ul>
+    </nav>
+</header>
